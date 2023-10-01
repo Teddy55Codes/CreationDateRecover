42499c3061d6f29d032713375a37f4d0214a9321
Teddy55Codes <teddy@teddy.email>
2023-10-01 19:19:45 +0200

    initial implementation

---
+++ CreationDateRecover/Program.cs
@@ -0,0 +1,55 @@
+﻿using System.Globalization;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace CreationDateRecover;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length == 0)
+        {
+            Console.WriteLine("Please provide a path to an image file.");
+            return;
+        }
+
+        string filePath = args[0];
+
+        try
+        {
+            DateTime? creationDate = GetImageCreationDate(filePath);
+            if (creationDate.HasValue)
+            {
+                File.SetCreationTime(filePath, creationDate.Value);
+                Console.WriteLine($"File creation date updated to {creationDate.Value}");
+            }
+            else
+            {
+                Console.WriteLine("Creation date not found in EXIF data.");
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine($"An error occurred: {e.Message}");
+        }
+    }
+
+    private static DateTime? GetImageCreationDate(string filePath)
+    {
+        using Image image = Image.FromFile(filePath);
+        PropertyItem[] propItems = image.PropertyItems;
+        foreach (PropertyItem propItem in propItems)
+        {
+            if (propItem.Id != 0x0132) continue;
+            string dateTaken = System.Text.Encoding.ASCII.GetString(propItem.Value).Trim();
+            DateTime creationDate;
+            if (DateTime.TryParseExact(dateTaken, "yyyy:MM:dd HH:mm:ss\0", CultureInfo.InvariantCulture, DateTimeStyles.None, out creationDate))
+            {
+                return creationDate;
+            }
+        }
+
+        return null;
+    }
+}
